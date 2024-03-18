7215a646426abb650226a237f04e27169703d0a3
Alfredo Martinez <ialfredo.mtz.rmz@gmail.com>
2024-03-18 15:46:13 -0600

    Feat: Add main data section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,12 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     // https://nuxt.com/modules/google-fonts
     '@nuxtjs/google-fonts',
+    // https://nuxt.com/modules/image
+    '@nuxt/image',
   ],
+  ui: {
+    icons: ['heroicons', 'ph', 'solar', 'fa6-brands'],
+  },
   googleFonts: {
     display: 'swap',
     download: true,
@@ -22,6 +27,12 @@ export default defineNuxtConfig({
       Inter: {
         wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
       },
+    },
+  },
+  image: {
+    provider: 'imagekit',
+    imagekit: {
+      baseURL: 'https://ik.imagekit.io/alfredom',
     },
   },
 });
