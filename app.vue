40c87afe1dadd0d100ccad16699c89b4759e84fa
Alfredo Martinez <ialfredo.mtz.rmz@gmail.com>
2024-03-18 16:02:55 -0600

    Feat: Add about section

--- app.vue
+++ app.vue
@@ -64,7 +64,7 @@
               </template>
             </UButton>
             <UButton
-              to="https://github.com/alfredomtzrmz"
+              to="https://www.linkedin.com/in/alfredomtzrmz/"
               target="_blank"
               color="white"
               variant="solid"
@@ -77,7 +77,7 @@
               </template>
             </UButton>
             <UButton
-              to="https://github.com/alfredomtzrmz"
+              to="https://twitter.com/alfredo_mtzrmz"
               target="_blank"
               color="white"
               variant="solid"
@@ -108,6 +108,18 @@
           />
         </span>
       </div>
+      <section class="flex min-h-0 flex-col gap-y-3">
+        <h2 class="text-xl font-bold">About</h2>
+        <p class="text-pretty font-mono text-sm text-gray-500">
+          My journey as a FrontEnd Engineer has been marked by multiple
+          successful transitions of products from the ground up. By leading
+          teams effectively, I ensure a workspace where teams can flourish,
+          adding to our collective ability to craft stunning products.
+          Currently, I work mostly with TypeScript, Vue, Nuxt, Node.js, and
+          Laravel. For over 7 years, I have been immersed in remote
+          collaborations with companies around the globe.
+        </p>
+      </section>
     </section>
   </main>
 </template>
